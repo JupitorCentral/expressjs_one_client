219ba75f4887384c34164a3a4763fc3eeb7601a6
jupitorSendsme <jupitorsendsme@gmail.com>
2025-02-11 19:40:37 +0900

    TypeScript Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  function Counter() {
+    // 제네릭으로 타입 명시적 지정
+    const [count, setCount] = useState<number>(0);
 
+    return (
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(prev => prev + 1)}>
+          Increment
+        </button>
+      </div>
+    );
+  }
   return (
-    <>
-      <h1>Vite + React + TypeScript</h1>
-    </>
+    <Counter />
   )
 }
 
